1da15678b49fc7001f26903e0de10e82051f58bd
povilastiskus <povilastiskus@gmail.com>
2023-10-03 09:22:37 +0300

    views saved

--- ptu16_library/library/views.py
+++ ptu16_library/library/views.py
@@ -4,26 +4,26 @@ from . import models
 
 def index(request):
     context = {
-        'num_books': models.Book.objects.count(),
-        'num_instances': models.BookInstance.objects.count(),
-        'num_available': models.BookInstance.objects.filter(status=0).count(),
-        'num_authors': models.Author.objects.count(),
-        'genres': models.Genre.objects.all(),
+        "num_books": models.Book.objects.count(),
+        "num_instances": models.Bookinstance.objects.count(),
+        "num_available": models.Bookinstance.objects.filter(status=0).count(),
+        "num_authors": models.Author.objects.count(),
+        "genres": models.Genre.objects.all(),
     }
-    return render(request, 'library/index.html', context)
+    return render(request, "library/index.html", context)
 
 
 def authors(request):
     return render(
         request, 
-        'library/author_list.html', 
-        {'author_list': models.Author.objects.all()}
+        "library/author_list.html", 
+        {"author_list": models.Author.objects.all()}
     )
 
 
 def author_detail(request, pk):
     return render(
         request,
-        'library/author_detail.html',
-        {'author': get_object_or_404(models.Author, pk=pk)}
+        "library/author_detail.html",
+        {"author": get_object_or_404(models.Author, pk=pk)}
     )
